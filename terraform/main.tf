1a8178cd0ec5d9cb1e576002765405418b947748
HappyPotatoLord <alexzzeni@hotmail.com>
2023-10-16 12:40:31 -0300

    refactor(terraform):  separates providers into a dedicated file and change proxmox provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,30 @@
-terraform {
-  backend "s3" {
-    bucket  = "proxmox-kubernetes"
-    key     = "terraform/terraform.tfstate"
-    region  = "ap-southeast-1"
-    profile = "dy2k"
-    encrypt = true
-  }
+# terraform {
+#   backend "s3" {
+#     bucket  = "proxmox-kubernetes"
+#     key     = "terraform/terraform.tfstate"
+#     region  = "ap-southeast-1"
+#     profile = "dy2k"
+#     encrypt = true
+#   }
 
-  required_providers {
-    proxmox = {
-      source  = "ondrejsika/proxmox"
-      version = "2020.9.21"
-    }
-  }
-}
+#  backend "local" {
+#    path = "./backend/terraform.tfstate"
+#  }
+# }
 
 provider "proxmox" {
   pm_tls_insecure = true
-  pm_api_url      = yamldecode(data.local_file.secrets.content).pm_api_url
-  pm_user         = yamldecode(data.local_file.secrets.content).pm_user
-  pm_password     = yamldecode(data.local_file.secrets.content).pm_password
-}
-
-provider "tls" {
-  version = "~> 2.1"
-}
-
-provider "random" {
-  version = "~> 2.2"
-}
-
-provider "local" {
-  version = "~> 1.4"
+  pm_api_url = yamldecode(data.local_file.secrets.content).pm_api_url
+  pm_user = yamldecode(data.local_file.secrets.content).pm_user
+  pm_password = yamldecode(data.local_file.secrets.content).pm_password
+  # pm_api_token_id = yamldecode(data.local_file.secrets.content).pm_api_token_id
+  # pm_api_token_secret = yamldecode(data.local_file.secrets.content).pm_api_token_secret
+  pm_parallel = 1
+  pm_debug = true
+  pm_log_enable = true
+  pm_log_file = "terraform-plugin-proxmox.log"
+  pm_log_levels = {
+    _default = "debug"
+    _capturelog = ""
+  }
 }
